947479bb5b9ce7b7755aede5830e7cf6ed990ac5
Ramesh <rameshw3offshore@gmail.com>
2018-09-05 12:10:11 +0530

    Get the data from users
    Get the data from users and shows the data authenticated people

---
+++ UsersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataAccess;
+using System.Web.Http.Cors;
+using System.Threading;
+
+namespace BasicAuthenticationAPI.Controllers
+{
+    [EnableCorsAttribute("*","*","*")]
+    public class UsersController : ApiController
+    {
+        [BasicAuthentication]
+        public HttpResponseMessage Get()
+        {
+            string username = Thread.CurrentPrincipal.Identity.Name;
+
+            using (mmb_dbEntities entities = new mmb_dbEntities())
+            {
+                //return entities.Users.ToList();
+                return Request.CreateResponse(HttpStatusCode.OK, entities.Users.ToList());
+
+            }
+        }
+    }
+}
