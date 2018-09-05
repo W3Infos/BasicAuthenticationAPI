01c434329330ec9a7ed83462659cf09fd1b3e353
Ramesh <rameshw3offshore@gmail.com>
2018-09-05 12:14:30 +0530

    Creating user security to return true of false by checking credentials
    Creating user security to return true of false by checking credentials And Create custom Authentication attribute to check user credentials.

---
+++ UserSecurity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataAccess;
+
+namespace BasicAuthenticationAPI
+{
+    public class UserSecurity
+    {
+
+        //Login method return true or false based on username and password matching cases
+        public static bool Login(string username, string password)
+        {
+            using (mmb_dbEntities entities = new mmb_dbEntities())
+            {
+                return entities.Users.Any(user => user.Username.Equals(username, StringComparison.OrdinalIgnoreCase) &&
+                    user.Password == password);
+            }
+        }
+
+    }
+}
